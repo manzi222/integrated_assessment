b2656dc75ad20fffcaab4262f09bd021c6ae6161
manzi222 <161708130+manzi222@users.noreply.github.com>
2024-11-14 05:11:09 -0800

    Add files via upload
    integrated_assessment

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const { json } = require('express/lib/response');
+const app=express();
+const port=3000;
+app.get('/',(req,res)=>{
+res.status(200).json({message:"post published by user"});
+});
+app.post('/',(req,res)=>{
+    res.status(200).json({message:"create post"});
+});
+app.listen(port,()=>{
+       console.log(`server is running at port: ${port}`);
+});
